bf84e5b168895498b0785bf711d243837981f3b5
Andrew Kuipers <deimors@gmail.com>
2019-01-25 21:15:46 -0600

    Sketch of Balloon model

---
+++ FloatMyHouse.Unity/Assets/Code/Model/Balloon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UniRx;
+
+public class Balloon
+{
+	private readonly IObservable<TimeTickEvent> _time;
+
+	private readonly ISubject<BalloonEvent> _events = new Subject<BalloonEvent>();
+	public IObservable<BalloonEvent> Events => _events;
+
+	private bool _addingLift;
+
+	public Balloon(IObservable<TimeTickEvent> time)
+	{
+		_time = time;
+
+		time.Where(_ => _addingLift)
+			.Subscribe(_ => _events.OnNext(new LiftAddedEvent()));
+	}
+
+	public void StartLift()
+	{
+		_addingLift = true;
+	}
+
+	public void EndLift()
+	{
+		_addingLift = false;
+	}
+}
+
+public abstract class TimeTickEvent
+{
+
+}
+
+public abstract class BalloonEvent
+{
+}
+
+public class LiftAddedEvent : BalloonEvent
+{
+
+}
