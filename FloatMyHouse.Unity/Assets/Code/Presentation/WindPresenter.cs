4c9cec285198c227f04a50f25875b11d978286ba
Andrew Kuipers <deimors@gmail.com>
2019-01-26 10:43:19 -0600

    Invisible Winds

---
+++ FloatMyHouse.Unity/Assets/Code/Presentation/WindPresenter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindPresenter : MonoBehaviour
+{
+	public SpriteRenderer WindSprite;
+
+	void Awake()
+	{
+		HideWind();
+	}
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		ShowWind();
+	}
+	
+	void OnTriggerExit2D(Collider2D collision)
+	{
+		HideWind();
+	}
+	
+	private void ShowWind()
+	{
+		WindSprite.enabled = true;
+	}
+
+	private void HideWind()
+	{
+		WindSprite.enabled = false;
+	}
+}
