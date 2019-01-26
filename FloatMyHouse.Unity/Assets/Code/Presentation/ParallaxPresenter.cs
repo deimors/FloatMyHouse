ae0b97d13e15be35d04eff1299fd1ace94734d4a
Andrew Kuipers <deimors@gmail.com>
2019-01-26 15:31:05 -0600

    Parallax WIP

---
+++ FloatMyHouse.Unity/Assets/Code/Presentation/ParallaxPresenter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxPresenter : MonoBehaviour
+{
+	private const string TextureName = "_MainTex";
+
+	public float Scale = .1f;
+	public MeshRenderer MeshRenderer;
+
+	private Vector2 _origOffset;
+
+	void Start()
+	{
+		_origOffset = MeshRenderer.sharedMaterial.GetTextureOffset(TextureName);
+	}
+
+	void LateUpdate()
+	{
+		var newOffset = new Vector2(transform.position.x * Scale, transform.position.y * Scale);
+		MeshRenderer.sharedMaterial.SetTextureOffset(TextureName, newOffset);
+	}
+}
