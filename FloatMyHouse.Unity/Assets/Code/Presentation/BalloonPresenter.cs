6674206d38e7b231392d96b15bacdf799f08f43d
Andrew Kuipers <deimors@gmail.com>
2019-01-25 20:57:21 -0600

    Balloon goes up!

---
+++ FloatMyHouse.Unity/Assets/Code/Presentation/BalloonPresenter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonPresenter : MonoBehaviour
+{
+	public Rigidbody2D BalloonRigidBody;
+	public float Magnitude;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			AddUpForce(Magnitude);
+		}
+
+		AddUpForce(1);
+	}
+
+	private void AddUpForce(float force)
+	{
+		BalloonRigidBody.AddForce(Vector2.up * force);
+	}
+}
