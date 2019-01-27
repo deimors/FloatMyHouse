efc33dba3f7e307e4ea1176359f6e9b6a8a656d8
Andrew Kuipers <deimors@gmail.com>
2019-01-27 13:17:40 -0600

    Coin Audio

---
+++ FloatMyHouse.Unity/Assets/Code/Presentation/CoinAudioPresenter.cs
@@ -0,0 +1,16 @@
+﻿using UniRx;
+using UnityEngine;
+using Zenject;
+
+public class CoinAudioPresenter : MonoBehaviour
+{
+	public AudioSource Source;
+
+	[Inject]
+	public void Initialize(Rewards rewardsModel)
+	{
+		rewardsModel.Events
+			.OfType<RewardsEvent, CoinAddedEvent>()
+			.Subscribe(_ => Source.Play());
+	}
+}
