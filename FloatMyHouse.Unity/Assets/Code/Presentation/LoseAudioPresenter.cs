6d2d621362aa7411a5b60031d772a2434b331e43
Andrew Kuipers <deimors@gmail.com>
2019-01-27 13:26:33 -0600

    Win and Lose Audio

---
+++ FloatMyHouse.Unity/Assets/Code/Presentation/LoseAudioPresenter.cs
@@ -0,0 +1,16 @@
+﻿using UniRx;
+using UnityEngine;
+using Zenject;
+
+public class LoseAudioPresenter : MonoBehaviour
+{
+	public AudioSource Source;
+
+	[Inject]
+	public void Initialize(Game gameModel)
+	{
+		gameModel.Events
+			.OfType<GameEvent, GameLostEvent>()
+			.Subscribe(_ => Source.Play());
+	}
+}
---
+++ FloatMyHouse.Unity/Assets/Code/Presentation/WinAudioPresenter.cs
@@ -0,0 +1,16 @@
+﻿using UniRx;
+using UnityEngine;
+using Zenject;
+
+public class WinAudioPresenter : MonoBehaviour
+{
+	public AudioSource Source;
+
+	[Inject]
+	public void Initialize(Game gameModel)
+	{
+		gameModel.Events
+			.OfType<GameEvent, GameWonEvent>()
+			.Subscribe(_ => Source.Play());
+	}
+}
